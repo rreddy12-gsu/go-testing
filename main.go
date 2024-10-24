6d72cf2d647922600b12a8db574694d59541c934
rreddy12-gsu <rreddy12@student.gsu.edu>
2024-10-24 15:27:37 +0000

    Initial commit

---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"fmt"
+	"slices"
+)
+
+type Item struct {
+	Name  string
+	Price float32
+}
+
+type Store struct {
+	Name  string
+	Items []Item
+}
+
+func listStores(stores []Store) []string {
+	storeNames := []string{}
+	for _, store := range stores {
+		if !slices.Contains(storeNames, store.Name) {
+			storeNames = append(storeNames, store.Name)
+		}
+	}
+	return storeNames
+}
+
+func cheapest(stores []Store, item string) Store {
+	cheapestStore := Store{}
+	var price float32 = 10000000
+	for _, store := range stores {
+		for _, i := range store.Items {
+			if i.Name == item {
+				if i.Price <= price {
+					price = i.Price
+					cheapestStore = store
+				}
+			}
+		}
+	}
+	return cheapestStore
+}
+
+func main() {
+	stores := []Store{
+		{
+			"Kroger",
+			[]Item{
+				{"Apple", 1.15},
+				{"Banana", 1.75},
+			},
+		},
+		{
+			"Aldi's",
+			[]Item{
+				{"Pizza", 5.00},
+				{"Lasagna", 4.25},
+				{"Banana", 1.00},
+			},
+		},
+		{
+			"Target",
+			[]Item{
+				{"Banana", 0.1},
+			},
+		},
+	}
+
+	// fmt.Println(stores)
+	for _, store := range stores {
+		msg := ""
+		msg += store.Name + " has: "
+		for _, item := range store.Items {
+			msg += item.Name + " for $" + fmt.Sprintf("%.2f", item.Price) + ", "
+		}
+		fmt.Println(msg[:len(msg)-2])
+	}
+	fmt.Println(listStores(stores))
+	fmt.Println(cheapest(stores, "Banana").Name)
+}
